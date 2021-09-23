b0d0682e77087d43858a977a84e9c56815bdfa2a
Lucatese <lucal2001@hotmail.com>
2021-09-23 01:36:13 -0400

    Drawing experiment: Drew a cone

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -11,6 +11,17 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500, 500);
+
+  background(250, 216, 20);
+
+  ellipseMode(CORNER);
+  ellipse(250,250,150,150)
+  ellipse(250,250,130,130)
+  ellipse(250,250,110,110)
+  ellipse(250,250,90,90)
+  ellipse(250,250,70,70)
+
 
 }
 
