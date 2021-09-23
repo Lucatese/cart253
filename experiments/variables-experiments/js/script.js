e174b46377dd0fa68df7701b71642a1db8a2fb88
Lucatese <lucal2001@hotmail.com>
2021-09-23 19:41:02 -0400

    Introduced JavaScript objects

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -10,17 +10,14 @@ author, and this description to match your project!
 
 
 let backgroundShade= 0;
-let circleSize= 200;
-let circleX=0;
-let circleY=250;
-let circleSpeed=2;
-let circleAcceleration=0.25;
-/**
-Description of preload
-*/
-function preload() {
 
-}
+
+let circle = {
+  x: 0,
+  y: 250,
+  size: 200,
+  speed: 2
+};
 
 /**
 Description of setup
@@ -35,9 +32,8 @@ Description of draw()
 */
 function draw() {
 background(backgroundShade);
-circleX += circleSpeed;
-circleSpeed += circleAcceleration;
-ellipse(circleX,circleY,circleSize);
+circle.x += circle.speed;
+ellipse(circle.x,circle.y,circle.size);
 
 
 }
