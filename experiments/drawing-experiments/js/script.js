41f5f4a2e03d4280c2a1aaff373d841d4a606cbb
Lucatese <lucal2001@hotmail.com>
2021-09-23 02:22:10 -0400

    Drawing experiment: added comments

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -1,45 +1,40 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+Drawing Experiments
+Luca Licatese
 
-Here is a description of this template p5 project.
+Experimenting with p5's drawing and color functions.
+
+Currently draws a face.
 **************************************************/
 
 // setup()
 //
-// Description of setup() goes here.
+// Draws a face on the canvas.
 function setup() {
   createCanvas(500, 500);
 
+// Set the background to turquoise
   background(0, 255, 200);
 
+// Draw a flesh-colored head
+fill(250,200,200);
+ellipse(250,250,200,200);
 
-noStroke();
+// Draw the eyes(black as the void of space)
+fill(0);
+ellipse(200,250,30,30);
+ellipse(300,250,30,30);
 
-  ellipseMode(CORNER);
-  fill(120,0,200,100);
-  ellipse(250,250,150,150);
-
-  fill(130,0,210,100);
-  ellipse(250,250,130,130);
-
-  fill(140,0,220,100);
-  ellipse(250,250,110,110);
-
-  fill(150,0,230,100);
-  ellipse(250,250,90,90);
-
-  fill(160,0,240,100);
-  ellipse(250,250,70,70);
-
-
+// Draw the mouth
+strokeWeight(10);
+line(200,300,300,300);
 }
 
 // draw()
 //
-// Description of draw() goes here.
+// Does nothing.
 function draw() {
 
 }
