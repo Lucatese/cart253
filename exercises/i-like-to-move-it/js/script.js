5f27aa5824572461ca668d5c4fa8762e494e8112
Lucatese <lucal2001@hotmail.com>
2021-09-23 23:05:32 -0400

    Changed background color and added some shapes
    I added some rectangles that place randomly across the top, a triangle which I'm still figuring out and a circle which would move across the screen

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -1,27 +1,34 @@
 /**
-Title of Project
-Author Name
+Exercise 1
+Luca Licatese
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This is the I like to move it exercise(1).
 */
 
 "use strict";
 
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0
+};
 
-/**
-Description of preload
-*/
-function preload() {
+let circle = {
+x: 0,
+y: 0,
+speed: 1.5,
+size: 150,
+fill: 140
 
 }
+
 
 
 /**
 Description of setup
 */
 function setup() {
-
+createCanvas(500,500);
 }
 
 
@@ -29,5 +36,22 @@ function setup() {
 Description of draw()
 */
 function draw() {
+background(bg.r,bg.g,bg.b);
+  bg.g = bg.g + 1;
+    bg.b = bg.b + 1;
 
+
+
+circle.x = circle.x + circle.speed;
+
+fill(circle.fill);
+ellipse(circle.x, circle.y,circle.size);
+
+
+
+  fill(255,0,0);
+  rect(random(500),random(250),15,15);
+
+  fill(149, 166, 45);
+  triangle(140, 420, 185, 380, 200, 420);
 }
