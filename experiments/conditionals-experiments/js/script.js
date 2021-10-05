7df637f6b9d963293679b8d5309859415e89217d
Lucatese <lucal2001@hotmail.com>
2021-10-04 21:45:32 -0400

    Experimented using loops for things besides counting

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -16,18 +16,32 @@ function preload() {
 
 }
 
-let caterpillar = {
-  x: 100,
-  y: 250,
-  segmentSize: 50
-}
+let circle = {
+  x: undefined,
+  y: undefined,
+  size: 100
+};
 
+let dangerZone = {
+  x: 250,
+  y: 250,
+  size: 150
+}
 /**
 Description of setup
 */
 function setup() {
 createCanvas(500,500);
 
+circle.x = random(0,width);
+circle.y = random(0,height);
+
+let d = dist(circle.x,circle.y,dangerZone.x,dangerZone.y);
+while (d < circle.size/2 + dangerZone.size/2) {
+  circle.x = random(0,width);
+  circle.y = random(0,height);
+  d = dist(circle.x,circle.y,dangerZone.x,dangerZone.y);
+  }
 }
 
 
@@ -36,25 +50,18 @@ Description of draw()
 */
 function draw() {
 background(0);
+
+//Danger Zone
+noFill();
+stroke(255,0,0);
+ellipse(dangerZone.x,dangerZone.y,dangerZone.size);
+
+
+
+fill(255);
 noStroke();
-fill(100,200,100);
+ellipse(circle.x,circle.y,circle.size);
 
-// let x = caterpillar.x;
-// let numSegments = 10;
-// let segmentsDrawn = 0;
-//
-// while (segmentsDrawn < numSegments) {
-//   ellipse(x,caterpillar.y,caterpillar.segmentSize);
-//   x = x + 40;
-//   segmentsDrawn++;
-//   }
 
-  let x = caterpillar.x;
-  let numSegments = 10;
-
-  for( let i = 0; i < numSegments; i++) {
-      ellipse(x,caterpillar.y,caterpillar.segmentSize);
-      x = x + 40;
-  }
 
 }
