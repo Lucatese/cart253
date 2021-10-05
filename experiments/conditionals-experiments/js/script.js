8c4c45e10e56ed97fa215e72ae0aa28203f8e9f6
Lucatese <lucal2001@hotmail.com>
2021-10-04 21:27:41 -0400

    started using loops

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -16,7 +16,11 @@ function preload() {
 
 }
 
-let displayCircle = false;
+let caterpillar = {
+  x: 100,
+  y: 250,
+  segmentSize: 50
+}
 
 /**
 Description of setup
@@ -31,15 +35,26 @@ createCanvas(500,500);
 Description of draw()
 */
 function draw() {
-
 background(0);
+noStroke();
+fill(100,200,100);
 
-if (mouseIsPressed) {
-  displayCircle = true;
-}
+// let x = caterpillar.x;
+// let numSegments = 10;
+// let segmentsDrawn = 0;
+//
+// while (segmentsDrawn < numSegments) {
+//   ellipse(x,caterpillar.y,caterpillar.segmentSize);
+//   x = x + 40;
+//   segmentsDrawn++;
+//   }
 
-if (displayCircle) {
-  ellipse(250,250,100,100);
+  let x = caterpillar.x;
+  let numSegments = 10;
+
+  for( let i = 0; i < numSegments; i++) {
+      ellipse(x,caterpillar.y,caterpillar.segmentSize);
+      x = x + 40;
   }
 
 }
