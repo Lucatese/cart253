9aad7e8209c51074db96ba315d1a8024f3caa0a8
Lucatese <lucal2001@hotmail.com>
2021-10-07 15:31:19 -0400

    Learned about naming functions

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Template p5 project
+Luca Licatese
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -16,11 +16,18 @@ function preload() {
 
 }
 
-
+let circle = {
+x: 0,
+y: 250,
+size: 100,
+vx: 1,
+vy: 0
+};
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(500,500);
 
 }
 
@@ -29,5 +36,35 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  move();
+  wrap();
+  display();
+}
+
+function wrap() {
+  if(circle.x > width) {
+    reset();
+  }
+}
+function display() {
+  fill(255,0,0);
+  ellipse(circle.x,circle.y,circle.size);
+}
+
+function move() {
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
+}
+
+function reset() {
+  circle.x = 0;
+  circle.vx = circle.vx + 2;
+  circle.vy = circle.vy - 0.25;
+  circle.size = circle.size + 5;
+}
+
+function mousePressed() {
+  reset();
 }
