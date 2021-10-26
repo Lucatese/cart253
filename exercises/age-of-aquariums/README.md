a249651a393a50715b825fa52c41f9fc4c93ab38
Lucatese <lucal2001@hotmail.com>
2021-10-25 23:38:38 -0400

    tried to get the fish to move randomly and  have them disappear when eaten but doesnt work
    code didnt work and im not really sure why

--- exercises/age-of-aquariums/README.md
+++ exercises/age-of-aquariums/README.md
@@ -1,3 +1,131 @@
 # Template p5 project
 
 This is the README file for the entire project. For more official projects you should write information here about the nature of the project, your name, any special explanations of how the project works, etc.
+
+let user = {
+  x: 0,
+  y: 0,
+  size: 100
+};
+
+let school = [];
+let schoolSize = 1;
+
+// Foods
+let food1;
+let food2;
+let food3;
+let food4;
+let food5;
+let food6;
+
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+function setup() {
+createCanvas(windowWidth,windowHeight);
+
+for (let i = 0; i < schoolSize; i++) {
+  let fish = createFood(random(0,width), random(0,height));
+  school.push(food);
+}
+
+}
+
+function createFood(x, y) {
+let food = {
+  x: x,
+  y: y,
+  size: 50,
+  vx: 0,
+  vy: 0,
+  speed: 2
+};
+return food;
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+background(0);
+
+
+// move the user with the mouse
+moveUser();
+
+
+
+// check whether the user has eaten either food
+checkFood(food1);
+checkFood(food2);
+checkFood(food3);
+checkFood(food4);
+checkFood(food5);
+checkFood(food6);
+
+// display the user and foods
+displayUser();
+
+
+for (let i = 0; i < school.length; i++) {
+  moveFood(school[i]);
+  displayFood(school[i]);
+  }
+
+}
+
+function moveFood(food) {
+  // choose whether to change direction
+  let change = random(0, 1);
+  if (change < 0.05) {
+    food.vx = random(-food.speed, food.speed);
+    food.vy = random(-food.speed, food.speed);
+  }
+
+  // move the fish
+  food.x = food.x + food.vx;
+  food.y = food.y + food.vy;
+
+  // constrain the fish to the canvas
+  food.x = constrain(food.x, 0, width);
+  food.y = constrain(food.y, 0, height);
+}
+// sets the user position to the mouse position
+function moveUser() {
+user.x = mouseX;
+user.y = mouseY;
+}
+
+function checkFood(food) {
+  if (!food.eaten) {
+    let d = dist(user.x, user.y, food.x, food.y);
+    if(d < user.size / 2 + food.size / 2) {
+      food.eaten = true;
+    }
+  }
+}
+
+
+  // draws the user as a circle
+  function displayUser() {
+    push();
+    fill(255);
+    ellipse(user.x, user.y, user.size);
+    pop();
+  }
+
+  function displayFood(food){
+    if (!food.eaten) {
+      push();
+      fill(255,100,100);
+      ellipse(food.x, food.y, food.size);
+      pop();
--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -8,113 +8,109 @@ author, and this description to match your project!
 
 "use strict";
 
-+"use strict";
 
-// our user which moves with mouse
+let school = [];
+let schoolSize = 10;
+
 let user = {
   x: 0,
   y: 0,
   size: 100
 };
 
-// Foods
-let food1;
-let food2;
-let food3;
-let food4;
-let food5;
-let food6;
-
-
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
+let fish1;
 
 function setup() {
-createCanvas(windowWidth,windowHeight);
+createCanvas(600,600);
 
-food1 = createFood(250,windowHeight/2);
-food2 = createFood(350,windowHeight/2);
-food3 = createFood(450,windowHeight/2);
-food4 = createFood(550,windowHeight/2);
-food5 = createFood(650,windowHeight/2);
-food6 = createFood(750,windowHeight/2);
+ for (let i = 0; i < schoolSize; i++) {
+   let fish = createFish(random(0,width), random(0,height));
+   school.push(fish);
+ }
 }
 
-function createFood(x,y) {
-  let food = {
+// createFish(x,y)
+// creates a new javascript object decribing a fish and returns it
+function createFish(x, y) {
+let fish = {
   x: x,
   y: y,
   size: 50,
-  eaten: false
-  };
-  return food;
+  vx: 0,
+  vy: 0,
+  speed: 2,
+  eaten:false
+};
+return fish;
 }
 
-/**
-Description of draw()
-*/
+
+// draw()
+// moves and displays our fish
 function draw() {
 background(0);
 
+// check whether the user has eaten either food
+checkFish(fish1);
 
-// move the user with the mouse
+
 moveUser();
 
-
-// check whether the user has eaten either food
-checkFood(food1);
-checkFood(food2);
-checkFood(food3);
-checkFood(food4);
-checkFood(food5);
-checkFood(food6);
-
-// display the user and foods
 displayUser();
-displayFood(food1);
-displayFood(food2);
-displayFood(food3);
-displayFood(food4);
-displayFood(food5);
-displayFood(food6);
 
+for (let i = 0; i < school.length; i++) {
+  moveFish(school[i]);
+  displayFish(school[i]);
+  }
 }
 
-// sets the user position to the mouse position
+// moveFish(fish)
+// chooses whether the provided fish changes direction and moves it
+function moveFish(fish) {
+  // choose whether to change direction
+  let change = random(0, 1);
+  if (change < 0.05) {
+    fish.vx = random(-fish.speed, fish.speed);
+    fish.vy = random(-fish.speed, fish.speed);
+  }
+
+  // move the fish
+  fish.x = fish.x + fish.vx;
+  fish.y = fish.y + fish.vy;
+
+  // constrain the fish to the canvas
+  fish.x = constrain(fish.x, 0, width);
+  fish.y = constrain(fish.y, 0, height);
+}
+
 function moveUser() {
 user.x = mouseX;
 user.y = mouseY;
 }
 
-function checkFood(food) {
-  if (!food.eaten) {
-    let d = dist(user.x, user.y, food.x, food.y);
-    if(d < user.size / 2 + food.size / 2) {
-      food.eaten = true;
+function checkFish(fish) {
+  if (!fish.eaten) {
+    let d = dist(user.x, user.y, fish.x, fish.y);
+    if(d < user.size / 2 + fish.size / 2) {
+      fish.eaten = true;
     }
   }
 }
 
+function displayUser() {
+  push();
+  fill(255);
+  ellipse(user.x, user.y, user.size);
+  pop();
+}
 
-  // draws the user as a circle
-  function displayUser() {
+// displayFish(fish)
+// displays the provided fish on the canvas
+function displayFish(fish){
+  if (!fish.eaten) {
     push();
-    fill(255);
-    ellipse(user.x, user.y, user.size);
+    fill(255,100,100);
+    ellipse(fish.x, fish.y, fish.size);
     pop();
   }
-
-  function displayFood(food){
-    if (!food.eaten) {
-      push();
-      fill(255,100,100);
-      ellipse(food.x, food.y, food.size);
-      pop();
-    }
-  }
+}
