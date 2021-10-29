3a5ddf327035cca7f8e44168ea74e56d2761c9f1
Lucatese <lucal2001@hotmail.com>
2021-10-28 21:15:52 -0400

    finished video 8.4

--- experiments/object-oriented-programming-experiments/js/Flower.js
+++ experiments/object-oriented-programming-experiments/js/Flower.js
@@ -5,9 +5,11 @@ class Flower {
     this.x = x;
     this.y = y;
     this.size = size;
+    this.maxSize = size;
     this.stemLength = stemLength;
     this.stemThickness = 10;
     this.petalThickness = 10;
+    this.maxPetalThickness = 10;
     // Color information
     this.stemColor = {
       r: 50,
@@ -15,13 +17,32 @@ class Flower {
       b: 50
     };
     this.petalColor = petalColor;
-
     this.centreColor = {
       r: 50,
       g: 0,
       b: 0
     };
+    this.alive = true;
   }
+
+shrink() {
+  let shrinkage = random(0,0.1);
+  this.size = this.size - shrinkage;
+  this.petalThickness = this.petalThickness - shrinkage/10;
+
+  if (this.size <= 0 || this.petalThickness <= 0) {
+    this.alive = false;
+  }
+}
+
+pollinate() {
+  let growth = random(0,0.5);
+  this.size = this.size + growth;
+  this.petalThickness = this.petalThickness + growth / 10;
+
+  this.size = constrain(this.size,0,this.maxSize);
+  this.petalThickness = constrain(this.petalThickness,0,this.maxPetalThickness);
+}
 
 display() {
   push();
@@ -35,14 +56,6 @@ display() {
   stroke(this.petalColor.r, this.petalColor.g, this.petalColor.b);
   ellipse(this.x, this.y, this.size);
   pop();
-  }
-
-  mousePressed() {
-    let d = dist(this.x,this.y,mouseX,mouseY);
-    if (d < this.size/2 + this.petalThickness) {
-      this.stemLength = this.stemLength + 5;
-      this.y = this.y - 5;
-    }
   }
 
 }
