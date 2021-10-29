d02fdbcc4160fc9747499c33a5f338024a22f498
Lucatese <lucal2001@hotmail.com>
2021-10-28 23:37:56 -0400

    Added sounds and images to exercise, also added states and end text

--- exercises/juggle-garden/js/script.js
+++ exercises/juggle-garden/js/script.js
@@ -7,9 +7,30 @@ Luca Licatese
 
 "use strict";
 
-
+// Defining force of gravity
 let gravityForce = 0.0025;
 
+
+let paddle;
+
+// Defining images and sounds
+let chainsawImage = undefined;
+
+let ballSFX = undefined;
+let chainsawSFX = undefined;
+
+/**
+Loading all images and sounds into code
+*/
+function preload() {
+  chainsawImage = loadImage("assets/images/chainsaw.png");
+
+  chainsawSFX = loadSound("assets/sounds/chainsawstart.wav");
+  ballSFX = loadSound("assets/sounds/ball.wav");
+
+}
+
+let state = `title`; // Can be either title, simulation, end1, end2
 
 /**
 Description of setup
@@ -17,6 +38,7 @@ Description of setup
 function setup() {
 createCanvas(windowWidth,windowHeight);
 
+paddle = new Paddle(300,20)
 }
 
 
@@ -25,5 +47,46 @@ Description of draw()
 */
 function draw() {
 background(0);
+
+
+
+ if (state === `title`) {
+   title();
+ } else if (state === `simulation`) {
+   simulation();
+ } else if (state === `end1`) {
+   end1();
+ } else if (state === `end2`) {
+   end2();
+ }
+}
+
+
+
+// Gameplay state
+function simulation() {
+  paddle.move();
+  paddle.display();
+  createChainsaws();
+  createBalls();
+  }
+
+// State that appears if a chainsaw falls out of frame
+function end1() {
+fill(255);
+textSize(32);
+textAlign(CENTER,CENTER);
+text(`Well that's gonna leave a scratch!`,windowWidth/2,windowHeight/2);
+}
+
+// State that appears if a ball falls out of frame
+function end2() {
+fill(255);
+textSize(32);
+textAlign(CENTER,CENTER);
+text(`Well at least it wasn't a chainsaw, Try Again!`,windowWidth/2,windowHeight/2);
+}
+
+
 
 }
