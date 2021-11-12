b22b87c93a0e205e7ce45d568a8d660bc05612d3
Lucatese <lucal2001@hotmail.com>
2021-11-12 02:14:11 -0500

    Brought a image for background and finished adding code for user

--- projects/project2/prototype/js/User.js
+++ projects/project2/prototype/js/User.js
@@ -2,18 +2,36 @@ class User {
   constructor(x,y,userImage) {
     this.x = x;
     this.y = y;
-    this.width =
-    this.height =
+    this.width = 200;
+    this.height = 150;
     this.dodged = true;
     this.image = userImage;
   }
 
+  // If the user gets hit by a dodgeableItem, dodged becomes false
+    checkHit(dodgeableItem) {
+      if (this.x > dodgeableItem.x - dodgeableItem.height / 2 &&
+        this.x > dodgeableItem.x - dodgeableItem.width / 2 &&
+        this.x < dodgeableItem.x + dodgeableItem.height / 2 &&
+        this.x < dodgeableItem.x + dodgeableItem.width / 2 &&
+        this.y > dodgeableItem.y - dodgeableItem.height / 2 &&
+        this.y > dodgeableItem.y - dodgeableItem.width / 2 &&
+        this.y < dodgeableItem.y + dodgeableItem.height / 2 &&
+        this.y < dodgeableItem.y + dodgeableItem.width / 2) {
+        this.dodged = false;
+      }
+    }
 
+    // Can control the user using mouse movement only for the Y-Axis
+    handleInput() {
+      this.y = mouseY;
+    }
 
-
-
-
-
-
-
+  // Displaying the user image
+   display() {
+     push();
+     imageMode(CENTER);
+     image(this.image, this.x, this.y, this.width, this.height);
+     pop();
+   }
 }
