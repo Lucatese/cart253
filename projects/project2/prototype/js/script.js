353f04272ff8420dbf0ea3045a94da52ffdf5891
Lucatese <lucal2001@hotmail.com>
2021-11-12 02:36:17 -0500

    Added code for setup and preload

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -18,14 +18,27 @@ let numBricks = 5;
 // Declaring the user variable
 let user;
 
+// Declaring the background image variable
+let bground;
 
+// Declaring all images that will be used
+let userImage = undefined;
 
+let brickImage = undefined;
+let wrenchImage = undefined;
+let dodgeballImage = undefined;
 
 /**
 Loading all images that will be used in code
 */
 function preload() {
+    brickImage = loadImage("assets/images/brick.png");
+    dodgeballImage = loadImage("assets/images/dodgeball.png");
+    wrenchImage = loadImage("assets/images/wrench.png");
 
+    userImage = loadImage("assets/images/user.png");
+
+    bg = loadImage("assets/images/gym.png");
 }
 
 
@@ -34,12 +47,48 @@ Creating the canvas to fit the window size
 */
 function setup() {
   createCanvas(windowWidth,windowHeight);
+
+  // Displaying user image
+ let x = width / 5;
+ let y = height / 2;
+ user = new User(x, y, userImage);
+
+ // Displaying bricks used for loop
+  for (let i = 0; i < numBricks; i++) {
+    let x = random(width / 2, width);
+    let y = random(0, height);
+    let brick = new Brick(x, y, brickImage);
+    dodgeableItems.push(brick);
+  }
+
+  // Displaying dodgeballs used for loop
+  for (let i = 0; i < numDodgeballs; i++) {
+    let x = random(width / 2, width);
+    let y = random(0, height);
+    let dodgeball = new Dodgeball(x, y, dodgeballImage);
+    dodgeableItems.push(dodgeball);
+  }
+
+  // Displaying wrenches used for loop
+  for (let i = 0; i < numWrenches; i++) {
+    let x = random(width / 2, width);
+    let y = random(0, height);
+    let wrench = new Wrench(x, y, wrenchImage);
+    dodgeableItems.push(wrench);
+  }
+
+
+// Declaring the direction and speed that the dodgeableItems will move in
+ for (let i = 0; i < dodgeableItem.length; i++) {
+   let dodgeableItem = dodgeableItems[i];
+   dodgeableItem.vx = dodgeableItem.speed;
+ }
 }
 
-
 /**
-Description of draw()
+Displays background image and creates the simulation, failure and success states
 */
 function draw() {
+  background(bground);
 
 }
