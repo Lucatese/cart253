b1f0e40acb361cfb7a30494bbfb3871073b7afab
Lucatese <lucal2001@hotmail.com>
2021-11-12 02:19:21 -0500

    Added code for brick, dodgeball and wrench classes

---
+++ projects/project2/prototype/js/Wrench.js
@@ -0,0 +1,18 @@
+class Wrench extends DodgeableItem {
+  constructor(x, y, image) {
+    super(x, y, image);
+    this.speed = -15;
+    this.width = 125;
+    this.height = 125;
+  }
+
+  // Displaying the Wrench image
+  display() {
+    super.display();
+
+    push();
+    imageMode(CENTER);
+    image(this.image, this.x, this.y, this.width, this.height);
+    pop();
+  }
+}
