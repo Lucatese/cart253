55376896dd999701194ecd0a086aeaeadbf934d7
Lucatese <lucal2001@hotmail.com>
2021-11-14 23:43:49 -0500

    Activity: Finished  the sound activity

--- activities/musical-toy/js/script.js
+++ activities/musical-toy/js/script.js
@@ -7,26 +7,41 @@ This is a program that plays music based on primitive physics!
 
 "use strict";
 
+// The balls
+let balls = [];
 
-/**
-Description of preload
-*/
-function preload() {
+// F-minor
+let notes = [`F3`,`G3`,`Ab4`,`Bb4`,`C4`,`Db4`,`Eb4`,`F4`];
 
-}
-
-
-/**
-Description of setup
-*/
+// setup()
+//
+// Just creates the canvas.
 function setup() {
+  createCanvas(600,600);
 
+  userStartAudio();
 }
 
-
-/**
-Description of draw()
-*/
+// draw()
+//
+// Description of draw() goes here.
 function draw() {
+  background(0);
 
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
+}
+
+function mousePressed() {
+  createBall(mouseX,mouseY);
+}
+
+function createBall(x,y) {
+  let note = random(notes);
+  let ball = new Ball(x,y,note);
+  balls.push(ball);
 }
