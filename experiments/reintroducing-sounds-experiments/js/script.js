87d305af0cb209cd6fe5da315108db8b84199840
Lucatese <lucal2001@hotmail.com>
2021-11-14 22:08:01 -0500

    Finished video 10.3

--- experiments/reintroducing-sounds-experiments/js/script.js
+++ experiments/reintroducing-sounds-experiments/js/script.js
@@ -8,39 +8,53 @@ author, and this description to match your project!
 
 "use strict";
 
-let oscillator;
-let t = 0;
+// Our synthesizer
+let synth;
+// The scale for F minor ("b" means "flat" if you haven't seen it before)
+let notes = [`F4`, `G4`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F5`];
+// The current note to play, start at the beginning
+let currentNote = 0;
+// To track the interval that plays note
+let interval;
 
-
-/**
-Description of setup
-*/
 function setup() {
-createCanvas(600,600);
-userStartAudio();
+  createCanvas(600, 600);
+  userStartAudio();
 
-oscillator = new p5.Oscillator(440,`sine`);
-oscillator.amp(0.2);
+  // Create the synthesizer
+  synth = new p5.PolySynth();
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
-background(0);
-
-let noiseValue = noise(t);
-let newFreq = map(noiseValue,-1,1,0,2000);
-oscillator.freq(newFreq);
-
-t = t + 0.1;
+  background(0);
 }
 
+// mousePressed() starts and stops our piano playing
 function mousePressed() {
-  oscillator.start();
+  // First check that the piano isn't already playing
+  // The interval will be undefined if it hasn't started
+  if (interval === undefined) {
+    // Start our interval, calling playNextNote every 500 milliseconds
+    // Assign the result to interval to remember the interval
+    interval = setInterval(playNextNote, 500);
+  }
+  else {
+    // If they click when it's playing, clear the interval and set interval
+    // back to undefined to stop play
+    clearInterval(interval);
+    interval = undefined;
+  }
 }
 
-function mouseReleased() {
-  oscillator.stop();
+// playNextNote() plays the next note in our array
+function playNextNote() {
+  // Chose the note at the current position
+  let note = notes[currentNote];
+  // Play it
+  synth.play(note, 0.2, 0, 0.4);
+  // Increase the current position and go back to 0 when we reach the end
+  currentNote = currentNote + 1;
+  if (currentNote === notes.length) {
+    currentNote = 0;
+  }
 }
