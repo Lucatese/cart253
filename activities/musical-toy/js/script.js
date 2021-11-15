66b02ad5f79649dc0f317cc47d01091a72dac62d
Lucatese <lucal2001@hotmail.com>
2021-11-14 23:17:20 -0500

    Activity: Started the sound activity

---
+++ activities/musical-toy/js/script.js
@@ -0,0 +1,32 @@
+/**
+Musical Toy
+Luca Licatese
+
+This is a program that plays music based on primitive physics!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
